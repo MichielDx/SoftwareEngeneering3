5dbed6020ea64e0229bb138d563d94cdbb6bf66c
MichielDx <michieldx@gmail.com>
2016-11-03 18:25:43 +0100

    fix

--- src/main/be/kdg/bagageafhandeling/transport/model/DTO/BagageMessageDTO.java
+++ src/main/be/kdg/bagageafhandeling/transport/model/DTO/BagageMessageDTO.java
@@ -1,28 +1,38 @@
 package main.be.kdg.bagageafhandeling.transport.model.DTO;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Created by Michiel on 3/11/2016.
  */
+@XmlRootElement(name="bagage")
 public class BagageMessageDTO {
-    private int BagageID;
-    private int ConveyorID;
+    @XmlElement
+    private int bagageID;
+    @XmlElement
+    private int conveyorID;
+
+    public BagageMessageDTO() {
+        
+    }
 
     public BagageMessageDTO(int bagageID, int conveyorID) {
-        BagageID = bagageID;
-        ConveyorID = conveyorID;
+        this.bagageID = bagageID;
+        this.conveyorID = conveyorID;
     }
 
     public int getBagageID() {
-        return BagageID;
+        return bagageID;
     }
 
     public int getConveyorID() {
-        return ConveyorID;
+        return conveyorID;
     }
 
     @Override
     public String toString() {
-        return "BagageId: " + BagageID + " | " + "ConveyorID " + ConveyorID;
+        return "BagageId: " + bagageID + " | " + "ConveyorID " + conveyorID;
     }
 }
 
