01c14667a181ba09ccee0f6b90544c385fed2ec2
MichielDx <michieldx@gmail.com>
2016-11-08 20:00:07 +0100

    hura

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package main.be.kdg.bagageafhandeling.transport;
+package main;
 
 import main.be.kdg.bagageafhandeling.transport.adapters.in.ConveyorServiceAPI;
 import main.be.kdg.bagageafhandeling.transport.adapters.in.RabbitMQIn;
